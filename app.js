226eb3bef1df2c0a76d3470e65c3c6d8202b9e19
Valts Vilcans <valts03@inbox.lv>
2020-06-08 14:35:48 +0200

    [added select pipe option]

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const pipeList = document.getElementById("pipeList");
+const pipeSelect = document.getElementById("pipeSelect");
 
 fetch("pipes.json")
   .then((res) => res.json())
   .then((data) => {
-    let output = "";
+    let listOutput = "";
+    let selectOutput = `<option value="" disabled selected hidden>Pipe ID</option>`;
     data.map((pipe) => {
       let status = "",
         issueText = "";
@@ -23,11 +25,17 @@ fetch("pipes.json")
       if (status === "warning" || status === "danger") {
         issueText = `<b>Issue:</b> ${pipe.issueHistory[0]}`;
       }
-      output += `
+      listOutput += `
       <li class="list-group-item d-flex justify-content-between align-items-center">
-      <div>ID: <b>${pipe.id}</b>   <span class="marg-l">${issueText}</span></div>
-      <span class="badge badge-primary bg-${status} marg-l">${pipe.status}</span></li>
+      <div>ID: <b>${pipe.id}</b>   <span class="ml-3">${issueText}</span></div>
+      <span class="badge badge-primary bg-${status} ml-3 badge-width">${pipe.status}</span></li>
             `;
+
+      selectOutput += `
+      <option value="${pipe.id}">${pipe.id}</option>
+      `;
     });
-    pipeList.innerHTML = output;
+    pipeList.innerHTML = listOutput;
+    pipeSelect.innerHTML = selectOutput;
+    console.log(selectOutput);
   });
